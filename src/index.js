dcc4ecb30f993ac6f3ee9b76e85dfc3bf07aef0a
Athul <athuldevan90@gmail.com>
2025-08-05 12:05:54 +0530

    feat :react-reduc install.UseSelelctor hook to acees the one reducer from store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
- import store from'./store'
+import "./index.css";
+import App from "./App";
 
- store.dispatch({type : 'account/deposit', payload : 5000})
- console.log(store.getState())
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import store from "./store";
+
+store.dispatch({ type: "account/deposit", payload: 5000 });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
